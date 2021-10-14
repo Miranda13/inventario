fb8d1febf7913df0e40f859dde964dbdc810d1ce
Jairo Baquero <jaialbaquero@gmail.com>
2021-10-14 14:01:43 -0500

    crud proveedores

--- app/app.py
+++ app/app.py
@@ -26,5 +26,13 @@ def editarUsuarios():
 def crearUsuarios():
     return render_template('usuarios/crear-usuarios.html')
 
+@app.route('/proveedores/editar')
+def editarProveedores():
+    return render_template('proveedores/editar-proveedores.html')
+
+@app.route('/proveedores/crear')
+def crearProveedores():
+    return render_template('proveedores/crear-proveedores.html')
+
 if __name__ == '__main__':
     app.run(debug = True, port = 8000)
